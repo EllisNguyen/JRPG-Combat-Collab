88ec2b169ca9b60cf9f2a3666d91c39ba4e5fdaf
deapkev <quanzi13579@gmail.com>
2021-12-01 13:57:57 +0700

    Audio related stuff

---
+++ COLLAB_2021/Assets/Scripts/Audio/AudioKeep.cs
@@ -0,0 +1,29 @@
+﻿///Author: Quan.TM
+///Description: Keep the audio through scenes.
+///Day created: 01/12/2021
+///Last edited: DD/MM/YYYY - editor name.
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AudioKeep : MonoBehaviour
+{
+    private static AudioKeep instance = null;
+    public static AudioKeep Instance
+    {
+        get { return instance; }
+    }
+   void Awake() 
+   {
+       if(instance != null && instance != this)
+       {
+           Destroy(this.gameObject);
+           return;
+       }
+       else
+       {
+           instance = this;
+       }
+       DontDestroyOnLoad(transform.gameObject);
+   }
+}
