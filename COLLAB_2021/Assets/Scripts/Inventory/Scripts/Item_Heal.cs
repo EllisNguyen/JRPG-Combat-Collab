b8415e207ead1c3970bfcae856a4c814650c7f77
miracleBouy <EllisTheMiracle@gmail.com>
2021-12-07 22:35:29 +0700

    fix major inventory bug

--- COLLAB_2021/Assets/Scripts/Inventory/Scripts/Item_Heal.cs
+++ COLLAB_2021/Assets/Scripts/Inventory/Scripts/Item_Heal.cs
@@ -21,76 +21,10 @@ public class Item_Heal : ItemBase
     [SerializeField] bool revive;
     [SerializeField] bool maxRevive;
 
-    public override bool Use(Creature creature)
+    public override bool Use(Character character)
     {
-        //USAGE OF REVIVE.
-        if (revive || maxRevive)
-        {
-            if (creature.HP > 0)
-                return false;
 
-            //Revive and restore half HP.
-            if (revive)
-                creature.IncreaseHP(creature.MaxHP / 2);
-            //Revive and restore full HP.
-            else if (maxRevive)
-                creature.IncreaseHP(creature.MaxHP);
+        return false;
 
-            //Cure all status.
-            creature.CureStatus();
-
-            return true;
-        }
-
-        //Deny usage of other items when creature is fainted.
-        if (creature.HP == 0)
-            return false;
-
-        //USAGE OF POTION.
-        if (restoreMaxHp || hpAmount > 0)
-        {
-            //Do nothing if creature's health is already full.
-            if (creature.HP == creature.MaxHP)
-                return false;
-
-            //Full potion.
-            if (restoreMaxHp)
-                creature.IncreaseHP(creature.MaxHP);
-            else
-                //else -> increase creature's HP.
-                creature.IncreaseHP(hpAmount);
-        }
-
-        //USAGE OF STATUS REMOVER.
-        if (recoverAllStatus || status != ConditionID.none)
-        {
-            //Check for status. Don't recover if no status.
-            if (creature.Status == null && creature.VolatileStatus == null) return false;
-
-            if (recoverAllStatus)
-            {
-                creature.CureStatus();
-                creature.CureVolatileStatus();
-            }
-            else
-            {
-                //Check if the right Recovery medicine match the current status.
-                if (creature.Status.Id == status) creature.CureStatus();
-                else if (creature.VolatileStatus.Id == status) creature.CureVolatileStatus();
-                else return false;
-            }
-        }
-
-        //USAGE OF PP RESTORER.
-        if (restoreMaxPp)
-        {
-            creature.Moves.ForEach(m => m.IncreasePP(m.Base.PP));
-        }
-        else if (ppAmount > 0)
-        {
-            creature.Moves.ForEach(m => m.IncreasePP(ppAmount));
-        }
-
-        return true;
     }
 }
