c52fe51793bc601ddfba9ac2b824c78a16e935e6
minhgau31 <52059331+minhgau31@users.noreply.github.com>
2022-05-09 23:27:59 +0700

    ingameshop

--- COLLAB_2021/Assets/ShopSystem/ItemInShop.cs
+++ COLLAB_2021/Assets/ShopSystem/ItemInShop.cs
@@ -2,17 +2,21 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using UnityEditor;
 
 public class ItemInShop : MonoBehaviour
 {
     [SerializeField] List<BuyList> BuyList;
     [SerializeField] GameObject BuyCanvas;
-    [SerializeField] Text Description;
-
+   // [SerializeField] Text Description;
+   // [SerializeField] Text ItemName;
+    [SerializeField] ItemBase item;
+    [SerializeField] SpriteRenderer graphic;
     // Start is called before the first frame update
     void Start()
     {
-        //Description.GetComponent<Text>.name
+      //  Description = GameObject.Find("Description").GetComponent<Text>();
+       // ItemName = GameObject.Find("ItemName").GetComponent<Text>();
     }
 
     // Update is called once per frame
@@ -20,17 +24,38 @@ public class ItemInShop : MonoBehaviour
     {
         
     }
-    //public void OnTriggerEnter(Collider player)
-    //{
-    //    if (player.name == "Player")
-    //    {
-    //        BuyCanvas.SetActive(true);
-    //        Description.text=
-    //    }
-    //}
+    public void OnTriggerEnter(Collider player)
+    {
+        if (player.name == "Player")
+        {
+            BuyCanvas.SetActive(true);
+            Debug.Log(BuyList[0].Item.ItemSprite.name);
+        }
+    }
+#if UNITY_EDITOR
+    void OnValidate()
+    {
+        graphic = GetComponentInChildren<SpriteRenderer>();
+        if (BuyList.Count == 0)
+        {
+            graphic.sprite = (Sprite)AssetDatabase.LoadAssetAtPath("Assets/Assets/UI/pickup_null.png", typeof(Sprite));
+            return;
+        }
 
+        if (BuyList.Count > 1)
+        {
+            graphic.sprite = (Sprite)AssetDatabase.LoadAssetAtPath("Assets/Assets/UI/pickup_pouch.png", typeof(Sprite));
+        }
+        else if (BuyList.Count == 1)
+        {
+            graphic.sprite = BuyList[0].Item.ItemSprite;
+        }
+    }
+#endif
 }
 
+
+[System.Serializable]
 public class BuyList
 {
     [SerializeField] ItemBase item;
