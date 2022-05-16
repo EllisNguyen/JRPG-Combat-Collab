1e6b73b9bbd0f9e78de7830df819b90fff7ef329
deapkev <83206696+deapkev@users.noreply.github.com>
2022-05-17 00:21:18 +0700

    Update audio scripts

--- COLLAB_2021/Assets/Scripts/Audio/AudioController.cs
+++ COLLAB_2021/Assets/Scripts/Audio/AudioController.cs
@@ -1,3 +1,8 @@
+///Author: Quan.TM
+///Description: Handle the audio control.
+///Day created: 16/05/2022
+///Last edited: DD/MM/YYYY - editor name.
+
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -8,17 +13,89 @@ namespace Audio
     {
         public static AudioController instance;
 
+        public bool debug;
+        public AudioTrack[] tracks;
 
+        private Hashtable audioTable; //relationship between audio types(key) and audio tracks(value)
+        private Hashtable jobTable; //relationship between audio types(key) and jobs(value) (Coroutine,IEnumerator)
+
+        [System.Serializable]
+        public class AudioObject
+        {
+            public AudioType type;
+            public AudioClip clip;
+        }
+
+        [System.Serializable]
+        public class AudioTrack
+        {
+            public AudioSource source;
+            public AudioObject[] audio;
+        }
         #region Unity Functions
+            private void Awake() 
+            {
+                //instance
+                if(!instance)
+                {
+                    Configure();
+                }
+            }
+
+            private void OnDisable()
+            {
+                Dispose();
+            }
 
         #endregion
 
         #region Public Functions
+            public void PlayAudio(AudioType type)
+            {
 
+            }
+
+            public void StopAudio(AudioType type)
+            {
+                
+            }
+
+            public void RestartAudio(AudioType type)
+            {
+                
+            }
         #endregion
 
         #region Private Functions
+            private void Configure()
+            {
+                instance = this;
+                audioTable = new Hashtable();
+                jobTable = new Hashtable();
+                GenerateAudioTable();
+            }
 
+            private void Dispose()
+            {
+
+            }
+
+            private void GenerateAudioTable()
+            {
+
+            }
+
+            private void Log(string msg)
+            {
+                if(!debug) return;
+                Debug.Log("[Audio Controller]: " + msg);
+            }
+
+            private void LogWarning(string msg)
+            {
+                if(!debug) return;
+                Debug.LogWarning("[Audio Controller]: " + msg);
+            }
         #endregion
     }
 }
